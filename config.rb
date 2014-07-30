f6e59c42012c3a23cf9d8132151b8725ddcd9e81
Frédéric Bouquet <bouquet.frederic@gmail.com>
2014-07-30 11:40:50 +0200

    Updated site

--- config.rb
+++ config.rb
@@ -8,13 +8,13 @@ http_path = "/"
 http_images_path = "/images"
 http_generated_images_path = "/images"
 http_fonts_path = "/fonts"
-css_path = "/home/bouquetf/projects/blog/grain-theme-octopress/.cache/compass/stylesheets"
+css_path = "/var/lib/jenkins/jobs/Push blog/workspace/.cache/compass/stylesheets"
 
 # Local development paths
-sass_path = "/home/bouquetf/projects/blog/grain-theme-octopress/theme/sass"
-generated_images_path = "/home/bouquetf/projects/blog/grain-theme-octopress/.cache/compass/images"
-images_path = "/home/bouquetf/projects/blog/grain-theme-octopress/theme/images"
-fonts_path = "/home/bouquetf/projects/blog/grain-theme-octopress/theme/fonts"
+sass_path = "/var/lib/jenkins/jobs/Push blog/workspace/theme/sass"
+generated_images_path = "/var/lib/jenkins/jobs/Push blog/workspace/.cache/compass/images"
+images_path = "/var/lib/jenkins/jobs/Push blog/workspace/theme/images"
+fonts_path = "/var/lib/jenkins/jobs/Push blog/workspace/theme/fonts"
 
 line_comments = false
 output_style = :compressed
